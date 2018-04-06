d30153f8ddad9c2aab5df5771ce6be21d440fd22
Dluoluo <luo.jiami@qq.com>
2018-04-06 17:37:15 +0800

    改善文件路径样式

--- src/components/page1/index.vue
+++ src/components/page1/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <app-header></app-header>
+    <app-hearder></app-hearder>
     <app-content></app-content>
     <app-footer></app-footer>
     <app-mask></app-mask>
@@ -9,14 +9,14 @@
 </template>
 
 <script>
-import appHeader from "../app-header";
-import appContent from "../app-content";
-import appFooter from "../app-footer";
-import appMask from "../app-Mask";
-import appAlert from "../app-alert";
+import appHearder from '../app-header'
+import appContent from '../app-content'
+import appFooter from '../app-footer'
+import appMask from '../app-mask'
+import appAlert from '../app-alert'
 export default {
   components: {
-    appHeader,
+    appHearder,
     appContent,
     appFooter,
     appMask,
